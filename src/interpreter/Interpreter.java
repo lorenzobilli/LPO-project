ff1469a4b58bdb59b40ddac7c13340407079b15b
Lorenzo Billi <lorebilli@outlook.it>
2017-07-03 17:10:54 +0200

    Interpreter now accepts input from user
    Just a very (simple) entry point for the project: Interpreter class will take input from user and print it in the standard output. An exception is thrown if an argument is given.

--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -1,10 +1,36 @@
 package interpreter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Interpreter class
  */
 public class Interpreter {
+
+    private static BufferedReader inputReader;
+
     public static void main(String[] args) {
+        inputReader = new BufferedReader(new InputStreamReader(System.in));
+        switch (args.length) {
+            case 0:
+                interactive();
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong argument(s) given");
+        }
+    }
+
+    private static void interactive() {
         System.out.print(">: ");
+        try {
+            String input = inputReader.readLine();
+            System.out.println(input);
+        } catch (IOException e) {
+            e.getMessage();
+            e.getCause();
+            e.printStackTrace();
+        }
     }
 }
