690093bd9e29348b358eb566eeaf652e26db6fc8
Lorenzo Billi <lorebilli@outlook.it>
2017-07-03 23:52:07 +0200

    Interpreter launcher completed
    Interpreter now follows project specifications completely (input from user/standard output, input from file/standard output, input from user/output to a file, input from a file/output to a file).

--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -1,21 +1,35 @@
 package interpreter;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * Interpreter class
  */
 public class Interpreter {
 
+    private static final String prompt = ">: ";
     private static BufferedReader inputReader;
+    private static BufferedWriter outputWriter;
 
     public static void main(String[] args) {
-        inputReader = new BufferedReader(new InputStreamReader(System.in));
         switch (args.length) {
             case 0:
                 interactive();
+                break;
+            case 1:
+                fromFile(args[0]);
+                break;
+            case 2:
+                if (!args[0].equals("-o")) {
+                    throw new IllegalArgumentException("Wrong argument(s) given");
+                }
+                toFile(args[1]);
+                break;
+            case 3:
+                if (!args[0].equals("-o")) {
+                    throw new IllegalArgumentException("Wrong arguments(s) given");
+                }
+                fromFileToFile(args[2], args[1]);
                 break;
             default:
                 throw new IllegalArgumentException("Wrong argument(s) given");
@@ -23,10 +37,69 @@ public class Interpreter {
     }
 
     private static void interactive() {
-        System.out.print(">: ");
+        inputReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(prompt);
         try {
             String input = inputReader.readLine();
             System.out.println(input);
+        } catch (IOException e) {
+            e.getMessage();
+            e.getCause();
+            e.printStackTrace();
+        }
+    }
+
+    private static void fromFile(String inputFileName) {
+        try {
+            inputReader = new BufferedReader(new FileReader(inputFileName));
+        } catch (IOException e) {
+            e.getMessage();
+            e.getCause();
+            e.printStackTrace();
+        }
+        try {
+            String line;
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.getMessage();
+            e.getCause();
+            e.printStackTrace();
+        }
+    }
+
+    private static void toFile(String outputFileName) {
+        inputReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(prompt);
+        try {
+            outputWriter = new BufferedWriter(new FileWriter(outputFileName));
+            outputWriter.write(inputReader.readLine());
+            outputWriter.newLine();
+            outputWriter.flush();
+        } catch (IOException e) {
+            e.getMessage();
+            e.getCause();
+            e.printStackTrace();
+        }
+    }
+
+    private static void fromFileToFile(String inputFileName, String outputFileName) {
+        try {
+            inputReader = new BufferedReader(new FileReader(inputFileName));
+            outputWriter = new BufferedWriter(new FileWriter(outputFileName));
+        } catch (IOException e) {
+            e.getMessage();
+            e.getCause();
+            e.printStackTrace();
+        }
+        try {
+            String line;
+            while ((line = inputReader.readLine()) != null) {
+                outputWriter.write(line);
+                outputWriter.newLine();
+                outputWriter.flush();
+            }
         } catch (IOException e) {
             e.getMessage();
             e.getCause();
