caf59d821fd1ce3866397402e15c119aa5412d29
Lorenzo Billi <lorebilli@outlook.it>
2017-07-09 21:34:03 +0200

    Environment class created inside common package
    Environment class will be used to store both static and dynamic environments of the program.

---
+++ src/common/Environment.java
@@ -0,0 +1,57 @@
+package common;
+
+import common.ast.Identity;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Environment class
+ */
+public class Environment<T> {
+
+    private List<Map<Identity, T>> localEnvironments = new LinkedList<>();
+
+    public Environment() {
+        enterScope();
+    }
+
+    public void enterScope() {
+        localEnvironments.add(0, new HashMap<>());
+    }
+
+    public void exitScope() {
+        localEnvironments.remove(0);
+    }
+
+    public T lookup(Identity identity) throws Exception {
+        return resolve(identity).get(identity);
+    }
+
+    public T update(Identity identity, T value) throws Exception {
+        return put(resolve(identity), identity, value);
+    }
+
+    public T newFresh(Identity identity, T value) {
+        Map<Identity, T> localEnvironment = localEnvironments.get(0);
+        return put(localEnvironment, identity, value);
+    }
+
+    private static <T> T put(Map<Identity, T> map, Identity identity, T value) {
+        return map.put(requireNonNull(identity), requireNonNull(value));
+    }
+
+    protected Map<Identity, T> resolve(Identity identity) throws Exception {
+        for (Map<Identity, T> localEnvironment : localEnvironments) {
+            if (localEnvironment.containsKey(identity)) {
+                return localEnvironment;
+            }
+        }
+        throw new Exception("Exception");   //TODO: throw a TypeChecker exception instead (when it'll implemented)
+    }
+
+}
